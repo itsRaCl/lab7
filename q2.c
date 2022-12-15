6da82e109351354aa101a7c73d5f3d7a65987ed9
itsRaCl <rajclerk.2004@gmail.com>
2022-12-15 21:26:05 +0530

    bug fix

--- q2.c
+++ q2.c
@@ -8,8 +8,9 @@ int linearSearch(int arr[10], int s){
 }
 
 int binarySearch(int arr[10], int s){
-	int f=1, ll = 0, ul=9, mid =4;
-	while (ll <= ul){
+	int ll = 0, ul=9,mid;
+	while (ll != ul){
+		mid = (ll + ul)/2;
 		if (arr[mid] ==s) return mid;
 		else if (arr[mid] > s) ul= mid-1;
 		else if (arr[mid] < s) ll = mid +1;
@@ -53,30 +54,41 @@ int main(){
 
 	while (sorted==0){
 		for (int i=0; i<9;i++){
-			if (arr[i]<arr[i+1]){
+			if (arr[i]>arr[i+1]){
 				int temp = arr[i+1];
 				arr[i+1]=arr[i];
 				arr[i]=temp;
 			}
 		}
+
+		for (int i=0; i<10; i++){
+			if (i==9){
+				sorted =1;
+			}
+			else if (arr[i]>arr[i+1]){
+				break;
+			}
+		}
 	}
+
+
 	printf("Sorted Array: \n");
 	for (int i=0; i<10;i++){
-		printf("Element %d: %d", i+1, arr[i]);
+		printf("Element %d: %d\n", i+1, arr[i]);
 	}
 
 	printf("Enter\n1 for linear Search\n2 for Binary Search\n3 for Selection Sort\n4 to exit\n>>");
 	scanf("%d", &o);
 	while (o!=4){
 		if (o==1){		
-			printf("Enter the number you want to search: ");
+			printf("Enter the number you want to search:");
 			scanf("%d",&s);
 			int i =linearSearch(arr, s);
 			if (i==-1){
 				printf("Element not found");
 			}
 			else{
-				printf("Element found at index: %d\n", i);
+				printf("Element found at index: %d\n", i+1);
 			}
 		}	
 		else if (o==2){
@@ -87,7 +99,7 @@ int main(){
 				printf("Element not found");
 			}
 			else{
-				printf("Element found at index: %d\n", i);
+				printf("Element found at index: %d\n", i+1);
 			}
 		}
 		else if (o==3){
@@ -105,5 +117,5 @@ int main(){
 		printf("Enter\n1 for linear Search\n2 for Binary Search\n3 for Selection Sort\n4 to exit\n>>");
 		scanf("%d", &o);
 	}
-	printf("\nBye Bye SearchNSort!");
+	printf("\nBye Bye SearchNSort!\n");
 }
