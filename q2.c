764c2e0cf56d322b0b382608be03e04e9d377ef5
itsRaCl <rajclerk.2004@gmail.com>
2022-12-15 21:15:24 +0530

    sorting array before running program

--- q2.c
+++ q2.c
@@ -9,27 +9,6 @@ int linearSearch(int arr[10], int s){
 
 int binarySearch(int arr[10], int s){
 	int f=1, ll = 0, ul=9, mid =4;
-
-	int sorted =0;
-
-	while (sorted==0){
-		for (int i=0; i<9;i++){
-			if (arr[i]>arr[i+1]){
-				int temp = arr[i+1];
-				arr[i+1]=arr[i];
-				arr[i]=temp;
-			}
-		}
-
-		for (int i=0; i<10; i++){
-			if (i==9){
-				sorted =1;
-			}
-			else if (arr[i]>arr[i+1]){
-				break;
-			}
-		}
-	}
 	while (ll <= ul){
 		if (arr[mid] ==s) return mid;
 		else if (arr[mid] > s) ul= mid-1;
@@ -69,9 +48,25 @@ int main(){
 		printf("Integer %d: ", i+1);
 		scanf("%d", &arr[i]);
 	}
+
+	int sorted =0;
+
+	while (sorted==0){
+		for (int i=0; i<9;i++){
+			if (arr[i]<arr[i+1]){
+				int temp = arr[i+1];
+				arr[i+1]=arr[i];
+				arr[i]=temp;
+			}
+		}
+	}
+	printf("Sorted Array: \n")
+	for (int i=0; i<10;i++){
+		printf("Element %d: %d", i+1, arr[i]);
+	}
+
 	printf("Enter\n1 for linear Search\n2 for Binary Search\n3 for Selection Sort\n4 to exit\n>>");
 	scanf("%d", &o);
-
 	while (o!=4){
 		if (o==1){		
 			printf("Enter the number you want to search: ");
